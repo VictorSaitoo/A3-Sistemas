6dd87598b8f00f08e6873953954331a29eb691bd
Victor Saito <129210511+VictorSaitoo@users.noreply.github.com>
2024-05-19 23:04:53 -0300

    commit

---
+++ task-api/readme.md
@@ -0,0 +1,16 @@
+LOGIN | CREATE USER
+
+    {
+	"email": "saito",
+	"password": "123456"
+    }
+
+CREATE TASK
+
+    {
+  "title": "teste sec",
+  "description": "teste0.23",
+  "expirationDate": "2024-01-01"
+    }
+
+http://localhost:3000
---
+++ task-api/src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { compareSync as bcryptCompareSync } from 'bcrypt';
+import { AuthResponseDTO } from './auth.DTO';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+    private jwtExpirationTimeInSeconds:number;
+
+    constructor(
+        private readonly    usersService: UsersService,
+        private readonly    jwtService: JwtService,
+        private readonly    configService: ConfigService
+    ){
+        this. jwtExpirationTimeInSeconds = +this.configService.get<number>('JWT_EXPIRATION_TIME');
+    }
+
+    signIn(email: string, password: string): AuthResponseDTO{
+        const foundUser = this.usersService.findByEmail(email);
+
+        if (!foundUser || !bcryptCompareSync(password, foundUser.password)){
+            throw new UnauthorizedException();
+        }
+
+        const payload = { sub: foundUser.id, email: foundUser.email}
+
+        const token = this.jwtService.sign(payload);
+
+        return { token, expiresIn: this.jwtExpirationTimeInSeconds }
+    }
+}
